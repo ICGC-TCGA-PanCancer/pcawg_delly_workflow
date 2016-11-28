951f36bc4f9a235e0b7244f5b31e064acdd6148d
Denis Yuen <denis.yuen@oicr.on.ca>
2016-11-28 15:58:42 -0500

    Strip out first whoami

--- delly_docker/scripts/start.sh
+++ delly_docker/scripts/start.sh
@@ -6,10 +6,7 @@ set -x
 
 gosu root chmod a+wrx /tmp
 gosu root chmod a+wrx /var/spool/cwl
-#export TMPDIR=/tmp
-#export HOME=/var/spool/cwl
 env
-whoami
 gosu seqware bash -c "$*"
 #allow cwltool to pick up the results created by seqware
 gosu root chmod -R a+wrx /var/spool/cwl
