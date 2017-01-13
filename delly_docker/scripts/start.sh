b6ab4e512556816d4d652244073b20d18fe575a0
Denis Yuen <denis.yuen@oicr.on.ca>
2017-01-13 13:47:06 -0500

    Feature/gosu and icgc portal (#11)
    * Add gosu
    
    * Add missed sudo
    
    * Functional retrofit in dev
    
    * Strip out first whoami
    
    * Redirect reference data

---
+++ delly_docker/scripts/start.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/env bash
+
+set -o errexit
+set -o pipefail
+set -x
+
+gosu root chmod a+wrx /tmp
+gosu root chmod a+wrx /var/spool/cwl
+env
+gosu seqware bash -c "$*"
+#allow cwltool to pick up the results created by seqware
+gosu root chmod -R a+wrx /var/spool/cwl
